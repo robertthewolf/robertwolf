9766413eee9ff90cb5dc022e9d98b42c107e721a
Robert Wolf <robert@Roberts-MacBook-Pro.local>
2018-11-13 15:42:17 -0800

    fixed gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,43 +8,50 @@ export default props => {
   return (
     <div>
       {props.data.items.map(item => (
-        <Fade
-        key={item.image.url}>
-        <figure
-          css={`
-            margin: ${rhythm(2)} auto;
-            max-width: 800px;
-            background-color: #1e1e1e;
-            border-radius: ${rhythm(.33)};
-            overflow: hidden;
-            padding-bottom: ${rhythm(2)}
-
-            img {
-              margin: 0;
-              width: 100%;
-            }
-          `}
-        >
-          {item.image.localFile !== null && item.image.localFile.childImageSharp !== null && (
-            <Img
-              sizes={item.image.localFile.childImageSharp.sizes}
-              alt={item.image.alt}
-            />
-          )}
-
-          {item.image.localFile !== null && item.image.localFile.childImageSharp === null && (
-            <img src={item.image.url} alt={item.image.alt} />
-          )}
-          <figcaption
+        <Fade key={item.image.url}>
+          <figure
             css={`
-              padding: ${rhythm(.75)} ${rhythm(.66)} ${rhythm(.66)};
-              text-align: center;
-              color: #dddddd;
+              margin: ${rhythm(2)} auto;
+              max-width: 800px;
+              background-color: #1e1e1e;
+              border-radius: ${rhythm(0.33)};
+              overflow: hidden;
+              padding-bottom: ${rhythm(2)};
             `}
           >
-            {item.caption.text}
-          </figcaption>
-        </figure>
+            {item.image.localFile !== null &&
+              item.image.localFile.childImageSharp !== null && (
+                <Img
+                  sizes={item.image.localFile.childImageSharp.sizes}
+                  alt={item.image.alt}
+                  css={`
+                    margin: 0;
+                    width: 100%;
+                  `}
+                />
+              )}
+
+            {item.image.localFile !== null &&
+              item.image.localFile.childImageSharp === null && (
+                <img
+                  src={item.image.url}
+                  alt={item.image.alt}
+                  css={`
+                    margin: 0;
+                    width: 100%;
+                  `}
+                />
+              )}
+            <figcaption
+              css={`
+                padding: ${rhythm(0.75)} ${rhythm(0.66)} ${rhythm(0.66)};
+                text-align: center;
+                color: #dddddd;
+              `}
+            >
+              {item.caption.text}
+            </figcaption>
+          </figure>
         </Fade>
       ))}
     </div>
